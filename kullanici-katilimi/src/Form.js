4025d9948bad2cbe4fe034d7733ccae0094370f2
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-08 23:15:20 +0300

    reset yapildi

--- kullanici-katilimi/src/Form.js
+++ kullanici-katilimi/src/Form.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import { useState } from 'react';
 
 const Form = styled.form`
 display: flex;
@@ -12,36 +12,77 @@ display: block;
 `;
 
 
+
 export default function FormDocument() {
+
+    const [data, setData] = useState([]);
+    const [person, setPerson] = useState({});
+
+    function handleChange(event) {
+        setPerson({ ...person, [event.target.name]: event.target.value })
+        console.log(person)
+    }
+
+    function submit(event) {
+        event.preventDefault();
+        setData([...data, person])
+        console.log(data)
+    }
+
+    function reset(event) {
+        setPerson({})
+        console.log(data)
+    }
+
     return (
-        <Form>
+        <Form onSubmit={submit}>
+            <h2>Formu eksiksiz doldurunuz.</h2>
             <div>
-            <label for="name-id">İsim</label>
-            <input type="text" id="name-id" name="name" />
-            </div>
-            
-            <div>
-            <label for="surname-id">Soyisim</label>
-            <input type="text" id="surname-id" name="surname" />
+                <label htmlFor="name-id">İsim</label>
+                <input type="text" id="name-id" name="name" value={person.name} onChange={handleChange} />
             </div>
 
             <div>
-            <label for="email-id">E-Mail</label>
-            <input type="email" id="email-id" name="email" />
+                <label htmlFor="surname-id">Soyisim</label>
+                <input type="text" id="surname-id" name="surname" value={person.surname} onChange={handleChange} />
             </div>
 
             <div>
-            <label for="password-id">Şifre</label>
-            <input type="password" id="password-id" name="password" />
+                <label htmlFor="email-id">E-Mail</label>
+                <input type="email" id="email-id" name="email" value={person.email} onChange={handleChange} />
             </div>
 
             <div>
-            <label for="checkbox-id">Kullanım Şartları</label>
-            <input type="checkbox" id="checkbox-id" name="checkbox" />
+                <label htmlFor="password-id">Şifre</label>
+                <input type="password" id="password-id" name="password" onChange={handleChange} />
             </div>
 
             <div>
-            <input type="submit" value="Submit" />
+                <label htmlFor="checkbox-id">Kullanım Şartları</label>
+                <input type="checkbox" id="checkbox-id" name="checkbox" onChange={handleChange} />
+            </div>
+
+            <div>
+                Bizi nereden duydunuz?
+                <label>
+                    Instagram
+                    <input type="radio" id="checkbox-1-id" name="checkbox" onChange={handleChange} />
+                </label>
+
+                <label>
+                    Linkedin
+                    <input type="radio" name="checkbox-2-id" onChange={handleChange} />
+                </label>
+
+                <label>
+                    Twitter
+                    <input type="radio" name="checkbox-3-id" onChange={handleChange} />
+                </label>
+            </div>
+
+            <div>
+                <button type="submit" id="submit-id">Gönder</button>
+                <button type="button" id="reset-id" onClick={reset}>Temizle</button>
             </div>
         </Form>
     )
