7e2f10611154691741f6f05b86aa98b216e08301
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-14 18:38:05 +0300

    Proje teslim

--- kullanici-katilimi/src/Form.js
+++ kullanici-katilimi/src/Form.js
@@ -17,32 +17,32 @@ display: block;
 const formSchema = Yup.object({
 
     firstname: Yup
-    .string()
-    .required("Bu alan zorunludur")
-    .matches(
-        /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
+        .string()
+        .required("Bu alan zorunludur")
+        .matches(
+            /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
             'Sadece latin harfleri kullanınız'
         ),
 
     surname: Yup
-    .string()
-    .required("Bu alan zorunludur")
-    .matches(
-        /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
+        .string()
+        .required("Bu alan zorunludur")
+        .matches(
+            /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
             'Sadece latin harfleri kullanınız'
         ),
 
     email: Yup
-    .string()
-    .required("Bu alan zorunludur")
-    .email("Lütfen geçerli bir mail giriniz"),
+        .string()
+        .required("Bu alan zorunludur")
+        .email("Lütfen geçerli bir mail giriniz"),
 
     password: Yup
-    .string()
-    .required("Bu alan zorunludur")
-    .min(8, 'Şifre en az 8 karakter uzunluğunda olmalı'),
+        .string()
+        .required("Bu alan zorunludur")
+        .min(8, 'Şifre en az 8 karakter uzunluğunda olmalı'),
 
-  });
+});
 
 
 
@@ -50,52 +50,49 @@ export default function FormDocument() {
 
     const [data, setData] = useState([]);
 
-    const initial = {firstname : '', surname : '', email : '', password: '', termsOfService : false, question : ''};
+    const initial = { firstname: '', surname: '', email: '', password: '', termsOfService: false, question: '' };
     const [person, setPerson] = useState(initial);
 
-    const {firstname, surname, email, password, termsOfService, question} = person;
+    const { firstname, surname, email, password, termsOfService, question } = person;
 
     const [buttonDisable, setButtonDisable] = useState(true);
 
     useEffect(() => {
         formSchema.isValid(person).then((valid) => setButtonDisable(!valid));
-      }, [person]);
+    }, [person]);
 
     const [formError, setFormError] = useState({
-        firstname : "",
-        surname : "",
-        email : "",
-        password : "",
+        firstname: "",
+        surname: "",
+        email: "",
+        password: "",
     });
-    
+
 
     function handleError(name, value) {
-        
+
         Yup.reach(formSchema, name)
-        .validate(value)
-        .then(() => {
-            console.log("ok");
-            setFormError({
-                ...formError,
-                [name] : ""
-            });
-        })
-        .catch((error) => {
-            console.log(error.errors[0]);
-            setFormError({
-                ...formError,
-                [name] : error.errors[0]
-            });
-        })
+            .validate(value)
+            .then(() => {
+                setFormError({
+                    ...formError,
+                    [name]: ""
+                });
+            })
+            .catch((error) => {
+                setFormError({
+                    ...formError,
+                    [name]: error.errors[0]
+                });
+            })
 
     }
 
     function handleChange(event) {
         (event.target.name === "firstname" || event.target.name === "surname" || event.target.name === "email" || event.target.name === "password") && handleError(event.target.name, event.target.value)
-        setPerson({ ...person, [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value});
+        setPerson({ ...person, [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value });
     }
 
-    console.log(person);
 
     function submit(event) {
         event.preventDefault();
@@ -106,22 +103,21 @@ export default function FormDocument() {
             }
         }
         setData([...data, person]);
-        
+
         axios.post('https://reqres.in/api/users', data)
-          .then(function (response) {
-            console.log(response);
-            setPerson(initial);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+            .then(function (response) {
+                console.log(response);
+                setPerson(initial);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
-    
+
 
     function reset() {
         setPerson(initial)
-        console.log(data)
     }
 
     return (
@@ -130,25 +126,25 @@ export default function FormDocument() {
             <div>
                 <label htmlFor="name-id">İsim</label>
                 <input type="text" id="name-id" name="firstname" value={firstname} onChange={handleChange} />
-                {formError.firstname !== "" && <div style={{color: "red", display: "inline", marginLeft: "0.5rem"}}>{formError.firstname}</div>}
+                {formError.firstname !== "" && <div style={{ color: "red", display: "inline", marginLeft: "0.5rem" }}>{formError.firstname}</div>}
             </div>
-            
+
             <div>
                 <label htmlFor="surname-id">Soyisim</label>
                 <input type="text" id="surname-id" name="surname" value={surname} onChange={handleChange} />
-                {formError.surname !== "" && <div style={{color: "red", display: "inline", marginLeft: "0.5rem"}}>{formError.surname}</div>}
+                {formError.surname !== "" && <div style={{ color: "red", display: "inline", marginLeft: "0.5rem" }}>{formError.surname}</div>}
             </div>
 
             <div>
                 <label htmlFor="email-id">E-Mail</label>
                 <input type="email" id="email-id" name="email" value={email} onChange={handleChange} />
-                {formError.email !== "" && <div style={{color: "red", display: "inline", marginLeft: "0.5rem"}}>{formError.email}</div>}
+                {formError.email !== "" && <div style={{ color: "red", display: "inline", marginLeft: "0.5rem" }}>{formError.email}</div>}
             </div>
 
             <div>
                 <label htmlFor="password-id">Şifre</label>
                 <input type="password" id="password-id" name="password" value={password} onChange={handleChange} />
-                {formError.password !== "" && <div style={{color: "red", display: "inline", marginLeft: "0.5rem"}}>{formError.password}</div>}
+                {formError.password !== "" && <div style={{ color: "red", display: "inline", marginLeft: "0.5rem" }}>{formError.password}</div>}
             </div>
 
             <div>
@@ -176,7 +172,7 @@ export default function FormDocument() {
 
             <div>
                 <button type="submit" id="submit-id" disabled={buttonDisable}>Gönder</button>
-                <button type="button" id="reset-id" onClick={reset}>Temizle</button>
+                <button type="button" id="reset-id" style={{ marginLeft: "0.5rem" }} onClick={reset}>Temizle</button>
             </div>
         </Form>
     )
